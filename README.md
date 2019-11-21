2399bb28e3a873cdfb83277e6a03dccc67fb5f02
Jason Biegel <jbiegel@shoprunner.com>
2019-11-21 01:27:17 -0500

    code

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Adafruit STEMMA Soil Sensor - I2C Capacitive Moisture Sensor
 
 [Tutorial](https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/python-circuitpython-test)
+
+
+```
+python3 -m pip install -r seesaw_soil_sensor/requirements.txt
+
+gardnr add metric soil moisture soil-moisture
+gardnr add metric soil moisture soil-temp
+
+gardnr add driver soil-sensor seesaw_soil_sensor.driver:SeesawSoilSensor
+```
--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
 import time
- 
-import busio 
+
+import busio
 from board import SCL, SDA
 from adafruit_seesaw.seesaw import Seesaw
 
@@ -9,15 +9,18 @@ from gardnr import drivers, metrics
 
 class SeesawSoilSensor(drivers.Sensor):
 
+    moisture_metric = 'soil-moisture'
+    t9e_metric = 'soil-temp'
+
     def setup(self):
         i2c_bus = busio.I2C(SCL, SDA)
         self.ss = Seesaw(i2c_bus, addr=0x36)
 
     def read(self):
         # read moisture level through capacitive touch pad
-        touch = ss.moisture_read()
- 
+        moisture = ss.moisture_read()
+        metrics.create_metric_log(self.moisture_metric, moisture)
+
         # read temperature from the temperature sensor
-        temp = ss.get_temp()
-
-
+        t9e = ss.get_temp()
+        metrics.create_metric_log(self.t9e_metric, t9e)
